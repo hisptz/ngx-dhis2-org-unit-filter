d739949b1b2eeee980be16b37265cd147cd5c874
Rajab Mkomwa <rajeyw@gmail.com>
2019-03-23 20:57:29 +0300

    fix: remove multiple rerendering of org unit tree item on item selection

--- projects/ngx-dhis2-org-unit-filter/src/lib/components/ngx-dhis2-org-unit-tree-item/ngx-dhis2-org-unit-tree-item.component.ts
+++ projects/ngx-dhis2-org-unit-filter/src/lib/components/ngx-dhis2-org-unit-tree-item/ngx-dhis2-org-unit-tree-item.component.ts
@@ -2,30 +2,31 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnChanges,
   OnInit,
   Output,
-  SimpleChanges
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectionStrategy
 } from '@angular/core';
 import { Store } from '@ngrx/store';
-import * as _ from 'lodash';
 import { Observable } from 'rxjs';
-import { first, take } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 
+import { getSelectedOrgUnitChildrenCount } from '../../helpers/get-selected-org-unit-children-count.helper';
+import { isOrgUnitSelected } from '../../helpers/is-org-unit-selected.helper';
 import { MINUS_CIRCLE_ICON, PLUS_CIRCLE_ICON } from '../../icons';
 import { OrgUnit } from '../../models';
 import { OrgUnitFilterState } from '../../store';
 import { getOrgUnitById } from '../../store/selectors/org-unit.selectors';
-import { isOrgUnitSelected } from '../../helpers/is-org-unit-selected.helper';
-import { getSelectedOrgUnitChildrenCount } from '../../helpers/get-selected-org-unit-children-count.helper';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'ngx-dhis2-org-unit-tree-item',
   templateUrl: './ngx-dhis2-org-unit-tree-item.component.html',
-  styleUrls: ['./ngx-dhis2-org-unit-tree-item.component.css']
+  styleUrls: ['./ngx-dhis2-org-unit-tree-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NgxDhis2OrgUnitTreeItemComponent implements OnInit {
+export class NgxDhis2OrgUnitTreeItemComponent implements OnInit, OnChanges {
   @Input() orgUnitId: string;
   @Input() expanded: boolean;
   @Input() selectedOrgUnits: any[];
@@ -49,27 +50,39 @@ export class NgxDhis2OrgUnitTreeItemComponent implements OnInit {
     this.selectedChildrenCount = 0;
   }
 
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    if (
+      simpleChanges.selectedOrgUnits &&
+      !simpleChanges.selectedOrgUnits.firstChange
+    ) {
+      console.log(simpleChanges, this.orgUnitId);
+    }
+  }
+
   ngOnInit() {
     if (this.orgUnitId) {
       // fetch current org unit
       this.orgUnit$ = this.store.select(getOrgUnitById(this.orgUnitId));
-
-      // get org unit selection status
-      this.selected = isOrgUnitSelected(this.orgUnitId, this.selectedOrgUnits);
-
-      this.orgUnit$.pipe(take(1)).subscribe((orgUnit: OrgUnit) => {
-        if (orgUnit) {
-          // Get count of selected children for this organisation unit
-          this.selectedChildrenCount = getSelectedOrgUnitChildrenCount(
-            orgUnit.children,
-            this.selectedOrgUnits
-          );
-
-          // Set expanded property for the current orgunits
-          this.expanded = !this.parentOrgUnit || this.selectedChildrenCount > 0;
-        }
-      });
+      this.setOrgUnitProperties();
     }
+  }
+
+  setOrgUnitProperties() {
+    // get org unit selection status
+    this.selected = isOrgUnitSelected(this.orgUnitId, this.selectedOrgUnits);
+
+    this.orgUnit$.pipe(take(1)).subscribe((orgUnit: OrgUnit) => {
+      if (orgUnit) {
+        // Get count of selected children for this organisation unit
+        this.selectedChildrenCount = getSelectedOrgUnitChildrenCount(
+          orgUnit.children,
+          this.selectedOrgUnits
+        );
+
+        // Set expanded property for the current orgunits
+        this.expanded = !this.parentOrgUnit || this.selectedChildrenCount > 0;
+      }
+    });
   }
 
   onToggleOrgUnitChildren(e) {
@@ -91,10 +104,16 @@ export class NgxDhis2OrgUnitTreeItemComponent implements OnInit {
   }
 
   onDeactivateOu(organisationUnit) {
+    console.log('here');
     this.deactivate.emit(organisationUnit);
   }
 
   onActivateOu(organisationUnit) {
+    console.log('activate');
     this.activate.emit(organisationUnit);
+  }
+
+  trackByFn(index, item) {
+    return item;
   }
 }
