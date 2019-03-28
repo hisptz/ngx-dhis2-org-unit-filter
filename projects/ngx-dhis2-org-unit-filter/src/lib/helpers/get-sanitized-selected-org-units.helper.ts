2ee1ce8a2a9f4f87be243f7df8ebd78c55d331cb
Rajab Mkomwa <rajeyw@gmail.com>
2019-03-28 22:07:01 +0300

    refactor: bump application version to 1.0.4 for org unit library

--- projects/ngx-dhis2-org-unit-filter/src/lib/helpers/get-sanitized-selected-org-units.helper.ts
+++ projects/ngx-dhis2-org-unit-filter/src/lib/helpers/get-sanitized-selected-org-units.helper.ts
@@ -4,7 +4,7 @@ import { USER_ORG_UNITS } from '../constants/user-org-units.constants';
 export function getSanitizedSelectedOrgUnits(selectedOrgUnits: any[]) {
   return isParentOrgUnitSelected(selectedOrgUnits)
     ? selectedOrgUnits
-    : (USER_ORG_UNITS || [])[0]
+    : USER_ORG_UNITS[0]
     ? [USER_ORG_UNITS[0], ...selectedOrgUnits]
     : selectedOrgUnits;
 }
