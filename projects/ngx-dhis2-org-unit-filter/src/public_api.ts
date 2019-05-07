dd23ad0c24f0e2d92efe635586a82611bb104b26
Rajab Mkomwa <rajeyw@gmail.com>
2019-05-07 15:53:27 +0300

    refactor(public_api.ts): make organisation unit constants publicly avaialable

--- projects/ngx-dhis2-org-unit-filter/src/public_api.ts
+++ projects/ngx-dhis2-org-unit-filter/src/public_api.ts
@@ -2,3 +2,5 @@
  * Public API Surface of ngx-dhis2-org-unit-filter
  */
 export * from './lib/ngx-dhis2-org-unit-filter.module';
+export * from './lib/constants/default-org-unit-filter-config.constant';
+export * from './lib/constants/user-org-units.constants';
