a7adffcd881f394af5c5348a39f14b01cc0bb296
Rajab Mkomwa <rajeyw@gmail.com>
2019-05-21 21:33:08 +0300

    refactor: export all models for orgunit

--- projects/ngx-dhis2-org-unit-filter/src/public_api.ts
+++ projects/ngx-dhis2-org-unit-filter/src/public_api.ts
@@ -4,3 +4,7 @@
 export * from './lib/ngx-dhis2-org-unit-filter.module';
 export * from './lib/constants/default-org-unit-filter-config.constant';
 export * from './lib/constants/user-org-units.constants';
+export * from './lib/models/org-unit-filter-config.model';
+export * from './lib/models/org-unit-group.model';
+export * from './lib/models/org-unit-level.model';
+export * from './lib/models/org-unit.model';
